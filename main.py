f3746fb5f61b270a050557a00786dd03a6152154
JH989876525 <ruihonghong1995@gmail.com>
2024-08-01 15:43:57 +0800

    refactor: refactor the code

--- main.py
+++ main.py
@@ -1,59 +1,72 @@
 import numpy as np
 
-class status:
-    def __init__(self, start):
-        self.summary = start
-
-    def __call__(self, addon):
-        self.summary = np.sum([self.summary, addon], axis = 0).tolist()
-
-class effort:
-    def __init__(self):
-        self.summary = [ 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    def __call__(self, addon):
-        self.summary = np.sum([self.summary, addon], axis = 0).tolist()
-
-class action:
-    hunting = [ 0, 1, 0, 2, 2, 0, 1, 1, 1]
-    workout = [ 0, 2,-1, 2, 1,-1, 2, 0,-1] # base line, changed base on status
-    picnic  = [ 3, 1, 2, 3,-2,-2, 0,-1, 0]
-    dating  = [ 0, 0, 2, 0, 0, 1, 3, 2, 0]
-    charity = [ 0, 0, 1, 1, 0, 2, 0, 2, 3]
-    sitdown = [ 1,-1, 1,-1, 0, 0, 1, 3, 0]
-    fishing = [ 1, 2, 0, 1, 0, 3, 0, 0, 0]
-    battleW = [ 0, 2, 1, 1, 2, 0, 1, 0, 0]
-    battleL = [ 3,-1, 3, 1, 0, 1, 0, 2, 1]
-    killing = [-1, 3,-1,-1, 2,-1, 2, 0,-1]
-
-    def __init__(self, status):
-        if (status[0] < 10*status[6]):
-            self.workout[status.index(max(status[1:8]))] = 4
-        else:
-            self.workout[0] = 4
+effort_list = ['hunting','workout','picnic','dating','charity','sitdown','fishing','battleW','battleL','killing']
+exp_list    = [15,15,15,15,15,15,15,15,15,15]
+effort_point = [
+    [ 0, 1, 0, 2, 2, 0, 1, 1, 1],
+    [ 0, 2,-1, 2, 1,-1, 2, 0,-1],
+    [ 3, 1, 2, 3,-2,-2, 0,-1, 0],
+    [ 0, 0, 2, 0, 0, 1, 3, 2, 0],
+    [ 0, 0, 1, 1, 0, 2, 0, 2, 3],
+    [ 1,-1, 1,-1, 0, 0, 1, 3, 0],
+    [ 1, 2, 0, 1, 0, 3, 0, 0, 0],
+    [ 0, 2, 1, 1, 2, 0, 1, 0, 0],
+    [ 3,-1, 3, 1, 0, 1, 0, 2, 1],
+    [-1, 3,-1,-1, 2,-1, 2, 0,-1]]
 
 class character:
-    def __init__(self, base, extra):
-        self.status = status(base)
-        self.extra = extra
-        self.effort = [ 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    def __init__(self,val_base,val_start,val_extra):
+        self.totalexp = 0
+        self.level = 1
+        self.efforts=[0,0,0,0,0,0,0,0,0]
+        self.base=val_base
+        self.status=val_start
+        self.extra=val_extra
 
     def show(self):
-        print("effort\t:", self.effort)
-        print("status\t:", self.status.summary)
-        print("extra\t:", self.extra)
-        self.result = np.sum([self.status.summary, self.extra], axis = 0).tolist()
-        print("summary\t:", self.result)
+        result = np.sum([self.status, self.extra], axis = 0).tolist()
+        print("Level\t:", self.level, ", Exp :", self.totalexp)
+        print("status\t:", result)
+        print("effort\t:", self.efforts)
 
-    def action(self, action, times):
+    def action(self, dowhat : str, times=1):
         for i in range(times):
-            print('aa')
+            # get efforts
+            index=effort_list.index(dowhat)
+            effort = effort_point[index]
+            if(dowhat == "workout"):
+                if (self.status[0] < self.status[6]*10):
+                    effort[status.index(max(status[1:8]))] = 4
+                else:
+                    effort[0] = 4
+            # add efforts
+            self.efforts = np.sum([self.efforts, effort], axis = 0).tolist()
+            # add to totalexp
+            self.totalexp += exp_list[index]
+            # self.level_check()
+
+    def action_effort(self, dowhat):
+        
+        return [0,0,0,0,0,0,0,0,0]
+
+    def level_check(self):
+        if(leveluped):
+			# calculate val
+			# add val to status
+            if(self.level<70):
+                self.level+=1
+            self.efforts=0
 
 
-list_base   = [ 25,  3,  2,  2,  3,  6,  2,  1,  1] # 基礎值
-list_start  = [350, 38, 33, 30, 25, 20, 32, 25, 20] # 初始值
-list_extra  = [  0,  0,  0,  0,  0,  0,  0,  0,121] # 額外值
-list_exp    = [100, 10, 10, 10, 10, 10,102,104, 10]
+val_base   = [ 25,  3,  2,  2,  3,  6,  2,  1,  1] # 基礎值
+val_start  = [350, 38, 33, 30, 25, 20, 32, 25, 20] # 初始值
+val_extra  = [  0,  0,  0,  0,  0,  0,  0,  0,121] # 額外值
+example    = [100, 10, 10, 10, 10, 10,102,104, 10]
 
-kirito = character(list_start, list_extra) # create a character
+kirito = character(val_base, val_start, val_extra)
 
-kirito.show() # show character status
+kirito.show() 
+
+kirito.action('hunting', 5)
+
+kirito.show() 
