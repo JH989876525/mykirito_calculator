3e722cea1a11a38e4aff9e608e4d2120cc817dd8
JH989876525 <ruihonghong1995@gmail.com>
2024-08-02 18:07:08 +0800

    feat: add float points to summary status

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ effort_point = [
     [ 0, 2, 1, 1, 2, 0, 1, 0, 0],
     [ 3,-1, 3, 1, 0, 1, 0, 2, 1],
     [-1, 3,-1,-1, 2,-1, 2, 0,-1]]
-leveling_exp_list = [0, 30, 60, 100, 150, 200, 250, 300, 370, 450, 500, 650, 800, 950, 1200, 1450, 1700, 1950, 2200, 2500, 2800, 3100, 3400, 3700, 4000, 4400, 4800, 5200, 5600, 6000, 6500, 7000, 7500, 8000, 8500, 9100, 9700, 10300, 11000, 11800, 12600, 13500, 14400, 15300, 16200, 17100, 18000, 19000, 20000, 21000, 23000, 25000, 27000, 29000, 31000, 33000, 35000, 37000, 39000, 41000, 44000, 47000, 50000, 53000, 56000, 59000, 62000, 65000, 68000, 71000]
+leveling_exp_list = [0, 30, 60, 100, 150, 200, 250, 300, 370, 450, 500, 650, 800, 950, 1200, 1450, 1700, 1950, 2200, 2500, 2800, 3100, 3400, 3700, 4000, 4400, 4800, 5200, 5600, 6000, 6500, 7000, 7500, 8000, 8500, 9100, 9700, 10300, 11000, 11800, 12600, 13500, 14400, 15300, 16200, 17100, 18000, 19000, 20000, 21000, 23000, 25000, 27000, 29000, 31000, 33000, 35000, 37000, 39000, 41000, 44000, 47000, 50000, 53000, 56000, 59000, 62000, 65000, 68000, 71000,0,0,0,0]
 
 class character:
     def __init__(self,val_base,val_start,val_extra):
@@ -24,12 +24,23 @@ class character:
         self.base=val_base
         self.status=val_start
         self.extra=val_extra
+        self.float=[0,0,0,0,0,0,0,0,0]
 
     def show(self):
-        result = np.sum([self.status, self.extra], axis = 0).tolist()
+        result=[0,0,0,0,0,0,0,0,0]
+        for i in range(9):
+            if (i == 0):
+                result[i] = self.status[i] + self.extra[i]*10 + self.float[i]*10
+            else:
+                result[i] = self.status[i] + self.extra[i] + self.float[i]
+                
+        # result = np.sum([self.status, self.extra], axis = 0).tolist()
+        # result = np.sum([result, self.float], axis = 0).tolist()
         print("Level\t:", self.level, ", TotalExp :", self.totalexp)
+        print("extra\t:", self.extra)
+        print("float\t:", self.float)
         print("status\t:", result)
-        print("effort\t:", self.efforts)
+        print("efforts\t:", self.efforts)
 
     def action(self, dowhat : str, times=1):
         for i in range(times):
@@ -45,23 +56,28 @@ class character:
             self.efforts = np.sum([self.efforts, effort], axis = 0).tolist()
             ### add to totalexp
             self.totalexp += exp_list[index]
-            ###
+            ### update level & add status
             self.level_update()
+            if self.level >= 70:
+                break
 
     def level_update(self):
-        while(self.totalexp >= leveling_exp_list[self.level] and self.level < 70):
-            print('level up')
+        while self.totalexp >= leveling_exp_list[self.level]:
+            print('level up', self.level, self.totalexp)
             ### base point
             self.status = np.sum([self.status, self.base], axis = 0).tolist()
             ### getting floatpoint? 
             point=self.floating_point_gain()
-			### floating point calculation
-            if(point!=0):
-                self.floating_point(point)
+			### floating point calculation & add
+            if point!=0:
+                gain = self.floating_point_add(point)
+                self.float = np.sum([self.float, gain], axis = 0).tolist()
 			### add val to status
             ### reset efforts & update level
             self.efforts=[0,0,0,0,0,0,0,0,0]
             self.level+=1
+            if self.level >= 70:
+                break
 
     def floating_point_gain(self):
         currentlevel=self.level
@@ -69,9 +85,11 @@ class character:
         point_gain = math.floor((finishlevel+1)/7) * (finishlevel+1-math.floor((finishlevel+1)/7)*7/2-7/2) - math.floor((currentlevel+1)/7) * (currentlevel+1-math.floor((currentlevel+1)/7)*7/2-7/2)
         return point_gain
 
-    def floating_point(self, point:int):
+    def floating_point_add(self, point:int):
         inttable=[[]]
         logtable=[[]]
+        sumtable=[[]]
+        float_sum=[0,0,0,0,0,0,0,0,0]
         for j in range(1,400):
             log2efforts=[0,0,0,0,0,0,0,0,0]
             tmp_efforts=[0,0,0,0,0,0,0,0,0]
@@ -144,8 +162,12 @@ class character:
                 else:result_effort[8]=1
             ### append to inttable
             inttable.append(result_effort)
+            ### recore sum of float point
+            if(point+0.5>j):
+                sumtable.append(result_effort)
+                float_sum = np.sum([float_sum, result_effort], axis = 0).tolist()
         ### sum of the inttable
-
+        return float_sum
 
 
 
@@ -154,7 +176,7 @@ class character:
 ### initail your kirito
 val_base   = [ 25,  3,  2,  2,  3,  6,  2,  1,  1] # 基礎值
 val_start  = [350, 38, 33, 30, 25, 20, 32, 25, 20] # 初始值
-val_extra  = [  0,  0,  0,  0,  0,  0,  0,  0,  0] # 額外值
+val_extra  = [  0,  0,  0,  0,  0,  0,  0,  0,121] # 額外值
 
 kirito = character(val_base, val_start, val_extra)
 
@@ -167,7 +189,5 @@ kirito.show()
 # kirito.show() 
 
 ### test level 6 to 7
-kirito.level=6
-kirito.totalexp = 245
-kirito.action('workout', 1)
+kirito.action('workout', 500)
 kirito.show()
